79c9662231baab0c7085d61f72d2329f9b7987c0
SeungWon <ysw8160@naver.com>
2021-05-06 00:10:57 +0900

    Update : LocationSetting View 4개 완성

--- app/src/main/java/com/example/capstonedesign/mainFeature/LocationSettingFragment.java
+++ app/src/main/java/com/example/capstonedesign/mainFeature/LocationSettingFragment.java
@@ -1,6 +1,5 @@
 package com.example.capstonedesign.mainFeature;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
@@ -33,7 +32,7 @@ public class LocationSettingFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_location_setting, container, false);
-        TextView tv = rootView.findViewById(R.id.edit_locationsetting_name);
+        TextView tv = rootView.findViewById(R.id.edit_locationsettingone_name);
         Button button = rootView.findViewById(R.id.btn_locationsetting_complete);
 
 
