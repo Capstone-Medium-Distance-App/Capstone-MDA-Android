b5e316a5bff19395e43ea01316d506f2a776bc8c
SeungWon <ysw8160@naver.com>
2021-05-21 15:04:02 +0900

    Update : MainSetting에 내위치 바로 띄우게 설정

--- app/src/main/java/com/example/capstonedesign/mainFeature/LocationMainFragment.java
+++ app/src/main/java/com/example/capstonedesign/mainFeature/LocationMainFragment.java
@@ -1,8 +1,11 @@
 package com.example.capstonedesign.mainFeature;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -38,9 +42,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -49,59 +62,57 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LocationMainFragment extends Fragment implements OnMapReadyCallback {
+
     private GoogleMap googleMap;
     private Retrofit retrofit;
     private MapView mapView;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private  double lat=0.0,log=0.0;
+    private double lat = 0.0, log = 0.0;
     private LatLng latLng;
-    public LocationMainFragment() { }
+    LocationManager locationManager;
+    LocationListener locationListener;
 
-    public static LocationMainFragment newInstance(){
+    public LocationMainFragment() {
+    }
+
+    int LOCATION_REQUEST_CODE = 10001;
+
+    public static LocationMainFragment newInstance() {
         LocationMainFragment fragment = new LocationMainFragment();
         return fragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_location_main, container, false);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_location_main, container, false);
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
                 .findFragmentById(R.id.mapView);
 
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-            if(getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if(location !=null){
-                            lat = location.getLatitude();
-                            log = location.getLongitude();
-                            Toast.makeText(getActivity(),lat+"   "+log,Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-            }else{
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION});
-            }
-        }
+//        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+//            if(getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+//                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        if(location !=null){
+//                            lat = location.getLatitude();
+//                            log = location.getLongitude();
+//                            Toast.makeText(getActivity(),lat+"   "+log,Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    }
+//                });
+//
+//            }else{
+//                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION});
+//            }
+//        }
 
 
         mapFragment.getMapAsync(this);
-
 
 
 //        mapView.getMapAsync(this);
@@ -148,27 +159,26 @@ public class LocationMainFragment extends Fragment implements OnMapReadyCallback
 //        }
 
 
-
         Button button = rootView.findViewById(R.id.btn_locationmain_test);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((LocationActivity)getActivity()).replaceFragment(new LocationDetailFragment());
+                ((LocationActivity) getActivity()).replaceFragment(new LocationDetailFragment());
             }
         });
         Button button2 = rootView.findViewById(R.id.btn_locationmain_test2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((LocationActivity)getActivity()).replaceFragment(new LocationFinalSelectFragment());
+                ((LocationActivity) getActivity()).replaceFragment(new LocationFinalSelectFragment());
             }
         });
 
 
 //        //Toolbar -> BackArrow + title + icons .version
-        Toolbar toolbar = (Toolbar)rootView.findViewById(R.id.toolbar);
-        ((LocationActivity)getActivity()).setSupportActionBar(toolbar);
-        ActionBar actionBar = ((LocationActivity)getActivity()).getSupportActionBar();
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((LocationActivity) getActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((LocationActivity) getActivity()).getSupportActionBar();
         actionBar.setTitle("Where To Go?");
         actionBar.setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼
         actionBar.setHomeAsUpIndicator(R.drawable.ic_tool_back); //뒤로가기버튼을 아이콘으로 변경
@@ -178,23 +188,35 @@ public class LocationMainFragment extends Fragment implements OnMapReadyCallback
         return rootView;
     }
 
-    private void requestPermissions(String[] strings) {
-    }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
         // 37.659627, 126.773459 정발산역
-            latLng = new LatLng(lat, log);
-            //현재위치를 가져와서
-            String userid = "123456789";
-            String userLat = "11.111111";
-            String userLong = "99.999999";
 
-            //camera가 바라보는 방향 정하기( 처음 위치 설정)
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            //camera 확대 정도
-            googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.clear();
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("승원이네"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            }
+        };
+        askLocationPermission();
+        // latLng = new LatLng(lat, log);
+
+//        //camera가 바라보는 방향 정하기( 처음 위치 설정)
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        //camera 확대 정도
+//        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        //현재위치를 가져와서
+        String userid = "123456789";
+        String userLat = "11.111111";
+        String userLong = "99.999999";
+
 
 //        CameraPosition cameraPosition = new CameraPosition.Builder()
 //                .target(new LatLng(lat, log))      // Sets the center of the map to Mountain View
@@ -204,38 +226,67 @@ public class LocationMainFragment extends Fragment implements OnMapReadyCallback
 //                .build();                   // Creates a CameraPosition from the builder
 //        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-            //처음에 접속했을때 자신의 위치를 서버로 보내는 코드 -kyu
-            Gson gson = new GsonBuilder()
-                    .setLenient()
-                    .create();
-            retrofit = new Retrofit.Builder()
-                    .baseUrl("http://ec2-3-37-60-253.ap-northeast-2.compute.amazonaws.com:8080/")
-                    //.baseUrl("http://192.168.35.225:8080/")
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-            MainFlowService mainFlowService = retrofit.create(MainFlowService.class);
+        //처음에 접속했을때 자신의 위치를 서버로 보내는 코드 -kyu
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://ec2-3-37-60-253.ap-northeast-2.compute.amazonaws.com:8080/")
+                //.baseUrl("http://192.168.35.225:8080/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        MainFlowService mainFlowService = retrofit.create(MainFlowService.class);
 
-            Call<userEnter> SendCall = mainFlowService.userEnter(userid, userLat, userLong);
+        Call<userEnter> SendCall = mainFlowService.userEnter(userid, userLat, userLong);
 
-            SendCall.enqueue(new Callback<userEnter>() {
-                @Override
-                public void onResponse(Call<userEnter> call, Response<userEnter> response) {
-                    System.out.println("userEnter DATA SEND SUCCESS!!!");
-                    System.out.println("=========================================================");
-                    Log.d("TAG", response.code() + "");
-                    Log.d("TAG", response.errorBody() + "");
-                    System.out.println(response);
-                    System.out.println("=========================================================");
+        SendCall.enqueue(new Callback<userEnter>() {
+            @Override
+            public void onResponse(Call<userEnter> call, Response<userEnter> response) {
+                System.out.println("userEnter DATA SEND SUCCESS!!!");
+                System.out.println("=========================================================");
+                Log.d("TAG", response.code() + "");
+                Log.d("TAG", response.errorBody() + "");
+                System.out.println(response);
+                System.out.println("=========================================================");
+            }
+
+            @Override
+            public void onFailure(Call<userEnter> call, Throwable t) {
+                t.printStackTrace();
+                System.out.println("userEnter DATA SEND FAIL!!!");
+            }
+        });
+    }
+
+    private void askLocationPermission() {
+        Dexter.withContext(requireActivity()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
                 }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-                @Override
-                public void onFailure(Call<userEnter> call, Throwable t) {
-                    t.printStackTrace();
-                    System.out.println("userEnter DATA SEND FAIL!!!");
-                }
-            });
-        }
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                latLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                googleMap.clear();
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("승원이네"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            }
 
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                    permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
