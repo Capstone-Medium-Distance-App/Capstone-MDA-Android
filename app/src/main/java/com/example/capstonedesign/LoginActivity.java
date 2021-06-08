ec0ac60a52d41455b2615800d65f1f522358e457
kyu9 <kyudo97@naver.com>
2021-06-08 10:03:13 +0900

    demo Testing

--- app/src/main/java/com/example/capstonedesign/LoginActivity.java
+++ app/src/main/java/com/example/capstonedesign/LoginActivity.java
@@ -4,12 +4,14 @@ import static com.example.capstonedesign.user.UserInfo.userName;
 import static com.example.capstonedesign.user.UserInfo.userId;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.capstonedesign.Retrofit.RetrofitClient;
+import com.example.capstonedesign.mainFeature.LocationActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,23 +28,25 @@ public class LoginActivity extends AppCompatActivity {
         EditText un = (EditText)findViewById(R.id.edit_UserName);
         EditText userPw = (EditText)findViewById(R.id.edit_UserPwd);
 
-        String uName = String.valueOf(un.getText());
 
-        Button btn = (Button)findViewById(R.id.btn_SignIn);
+
+        Button btn = findViewById(R.id.btn_SignIn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String uName = un.getText().toString();
+                System.out.println(uName);
                 userName = uName;
-                Call<String> call = rc.mainFlowService.userLogin(userName);
+                Call<String> call = rc.mainFlowService.userLogin(uName);
                 call.enqueue(new Callback<String>() {
                     @Override
                     public void onResponse(Call<String> call, Response<String> response) {
-                        if(response.isSuccessful()){
-                            System.out.println("=====================================");
-                            System.out.println(response.body());
-                            userId = Integer.parseInt(response.body());
-                            System.out.println("=====================================");
-                        }
+                        System.out.println("=====================================");
+                        System.out.println(response.body());
+                        userId = Integer.parseInt(response.body());
+                        System.out.println("=====================================");
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
                     }
 
                     @Override
