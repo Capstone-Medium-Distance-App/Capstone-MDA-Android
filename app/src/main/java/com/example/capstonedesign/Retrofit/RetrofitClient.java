1b9a9c9f0b5e8d5ea7d72dcfa796d191e8f87e59
kyu9 <kyudo97@naver.com>
2021-06-17 12:36:48 +0900

    Testing

--- app/src/main/java/com/example/capstonedesign/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/capstonedesign/Retrofit/RetrofitClient.java
@@ -12,7 +12,6 @@ public class RetrofitClient {
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://ec2-3-37-60-253.ap-northeast-2.compute.amazonaws.com:8080/")
             //.baseUrl("http://192.168.35.225:8080/")
-//            .baseUrl("http://192.168.35.225:8080/")
             //.baseUrl("http://192.168.35.71:8080/")
             //.baseUrl("http://192.168.219.109:8080/")
             .addConverterFactory(GsonConverterFactory.create(gson))
